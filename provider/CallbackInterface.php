cd2cad975288604629ff5fc2775ffda916098670
Roman Usenko <ninazu@ukr.net>
2016-01-29 17:02:25 +0200

    Update CallbackInterface.php

--- provider/CallbackInterface.php
+++ provider/CallbackInterface.php
@@ -4,6 +4,10 @@ namespace ninazu\unwanted\provider;
 
 interface CallbackInterface {
 
+	public function syncLists($lists);
+
+	public function getChunk($chunk_type_cid, $list_id);
+
 	public function resetDatabase();
 
 	public function getClientVersion();
